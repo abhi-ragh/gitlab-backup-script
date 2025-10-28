bea347767df4b7d247bc69f9cb3591300b79f4c5
Abhiragh A R <abhiragh0@gmail.com>
2025-10-28 17:12:28 +0530

    Change Import logic to import select files

--- import.py
+++ import.py
@@ -1,11 +1,20 @@
-def import_projects(id):
+import os 
+import requests
+import time 
+from datetime import date
+import tkinter as tk
+from tkinter import filedialog
+
+from projects import projects,headers
+
+def import_projects(path, name):
     print("\nStarting Import")
     data = {
-        "path":f"{projects[id]}_{date.today()}.tar.gz"
+        "path": name
     }
     print("Waiting for Import to finish")
-    try:abhiragh0/portfolio_2025-10-28.tar.gz
-        files = { "file": open(f"{directory_name}/{projects[id]}_backup.tar.gz", 'rb') }
+    try:
+        files = { "file": open(path, 'rb') }
         response = requests.post(f"https://gitlab.com/api/v4/projects/import", headers=headers, files=files, data=data)
         new_project_id = response.json()["id"]
         
@@ -20,11 +29,37 @@ def import_projects(id):
         
         print("Import Successful\n")
     except Exception as e:
-        print(response.json())abhiragh0/portfolio_2025-10-28.tar.gz
+        print(response.json())
     
+
+def get_file_path():
+
+    root = tk.Tk()
+    root.withdraw() 
+    
+    print("Opening file dialog...")
+    file_path = filedialog.askopenfilename()
+    
+    root.destroy()
+    
+if file_path:  
+        file_name = os.path.basename(file_path)
+        return file_path, file_name
+    else:
+        return None, None
+
 def main():
-    for id in projects.keys():
-        export_projects(id)
+    
+    print("Select File to Import")
+    
+    path, name = get_file_path()
+
+    if path and name:
+        import_projects(path, name)
+
+    else:
+        print("\nNo file was selected (dialog was canceled).")
+
 
 if __name__ == "__main__":
     main()
